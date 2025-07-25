3abf77104b93bfd25beead39da5b813adc942c1f
Ben Spiers <ben.spiers22@gmail.com>
2025-07-25 21:48:24 +0100

    Start from bevy_new_2d example template
    Removed audio stuff because large files

---
+++ src/demo/level.rs
@@ -0,0 +1,23 @@
+//! Spawn the main level.
+
+use bevy::prelude::*;
+
+use crate::{
+    demo::player::{PlayerAssets, player},
+    screens::Screen,
+};
+
+/// A system that spawns the main level.
+pub fn spawn_level(
+    mut commands: Commands,
+    player_assets: Res<PlayerAssets>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    commands.spawn((
+        Name::new("Level"),
+        Transform::default(),
+        Visibility::default(),
+        StateScoped(Screen::Gameplay),
+        children![player(400.0, &player_assets, &mut texture_atlas_layouts),],
+    ));
+}
